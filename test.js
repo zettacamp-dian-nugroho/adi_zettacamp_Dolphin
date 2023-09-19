41610070f35844e595669bcaa6beea38f5e8c632
zettacamp-dian-nugroho <dian.nugroho@zettacamp.pro>
2023-09-19 19:45:22 +0700

    JS_Day9 | Day-18

---
+++ test.js
@@ -0,0 +1,10 @@
+/** @format */
+
+function ConditionalTest(x) {
+  if (x) {
+    console.log('Ini Benar Didalam If()');
+  } else {
+    console.log('Ini Berada Didalam Else{}');
+  }
+}
+ConditionalTest(true)
