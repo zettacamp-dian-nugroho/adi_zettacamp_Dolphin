94415fe51072383c7fc48e4666a86112d97406d0
zettacamp-dian-nugroho <dian.nugroho@zettacamp.pro>
2023-09-19 19:38:48 +0700

    JS_Day7 | Day15

---
+++ index.js
@@ -0,0 +1,269 @@
+/** @format */
+
+const dataLagu = [
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH',
+    genre: 'Pop',
+    title: 'Feat-BCL-Mencari-Cinta',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 3.33,
+  },
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH',
+    genre: 'Rock',
+    title: 'Jalani-Mimpi',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 3.58,
+  },
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH1',
+    genre: 'Metal',
+    title: 'Kau-Udara-Bagiku',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 4.26,
+  },
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH1',
+    genre: 'Pop',
+    title: 'Mendekati-Lugu',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 3.3,
+  },
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH2',
+    genre: 'Rock',
+    title: 'Menemaniku',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 3.33,
+  },
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH2',
+    genre: 'Metal',
+    title: 'My-Situation',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 4.34,
+  },
+  {
+    album: 'Keterkaitan Keterikatan',
+    artist: 'NOAH',
+    genre: 'Pop',
+    title: 'Wanitaku',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 4.25,
+  },
+  {
+    album: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    genre: 'Rock',
+    title: 'Another One Bites the Dust',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 3.42,
+  },
+  {
+    album: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    genre: 'Metal',
+    title: 'Bohemian Rhapsody',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.59,
+  },
+  {
+    album: 'Bohemian Rhapsody',
+    artist: 'Queen1',
+    genre: 'Pop',
+    title: `Don't Stop Me Now`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 3.32,
+  },
+  {
+    album: 'Bohemian Rhapsody',
+    artist: 'Queen1',
+    genre: 'Rock',
+    title: 'I Want to Break Free',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 4.32,
+  },
+  {
+    album: 'Bohemian Rhapsody',
+    artist: 'Queen2',
+    genre: 'Metal',
+    title: 'We Will Rock You',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 2.15,
+  },
+  {
+    album: 'Bohemian Rhapsody',
+    artist: 'Queen2',
+    genre: 'Pop',
+    title: 'Somebody To Love',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.1,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses',
+    genre: 'Rock',
+    title: 'Estranged',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 9.48,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses',
+    genre: 'Metal',
+    title: 'Patience',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.57,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses1',
+    genre: 'Pop',
+    title: `Knockin' On Heaven's Door`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.36,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses1',
+    genre: 'Rock',
+    title: 'Welcome to the Jungle',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 4.4,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses2',
+    genre: 'Metal',
+    title: `Sweet Child O' Mine`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.03,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses2',
+    genre: 'Pop',
+    title: 'November Rain',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 9.17,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses3',
+    genre: 'Rock',
+    title: `Don't Cry`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.14,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses3',
+    genre: 'Metal',
+    title: `Don't Cry`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.14,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Fourtwnty',
+    genre: 'Rock',
+    title: 'Zona Nyaman',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 9.48,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Guns N Roses',
+    genre: 'Metal',
+    title: 'Patience',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.57,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Banda Neira',
+    genre: 'Pop',
+    title: `Yang Patah Tumbuh, yang Hilang Berganti`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.36,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Pusakata',
+    genre: 'Rock',
+    title: 'Pejamkan Matamu',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 4.4,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Payung Teduh',
+    genre: 'Metal',
+    title: `Akad`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.03,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Sheila on 7',
+    genre: 'Pop',
+    title: 'Dan',
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 9.17,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Ungu',
+    genre: 'Rock',
+    title: `Saat Bahagia`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.14,
+  },
+  {
+    album: 'Estranged',
+    artist: 'Rizky Febian',
+    genre: 'Metal',
+    title: `Cuek`,
+    cover_artwork: 'https://i.scdn.co/image/ab67616d0000b2',
+    duration: 5.14,
+  },
+];
+
+function groupByArtist(dataLagu, artist) {
+  const tempArtist = dataLagu.filter((list) => list.artist == artist || list.artist.toLowerCase() == artist);
+  return tempArtist.length ? tempArtist : 'Artist tidak tersedia';
+}
+
+function groupByGenre(dataLagu, genre) {
+  const tempGenre = dataLagu.filter((list) => list.genre == genre || list.genre.toLowerCase() == genre);
+  return tempGenre.length ? tempGenre : 'Genre tidak tersedia';
+}
+
+function groupSongUnder60Minute(dataLagu, howmany) {
+  let totalDuration = 0;
+  let randomArtist = [];
+
+  for (let i = 0; i < howmany; i++) { //harus diperbaiki kembali jika ada data jutaan
+    const randomIndex = Math.floor(Math.random() * dataLagu.length);
+    const tempDurasi = dataLagu[randomIndex].duration;
+    const tempTotal = totalDuration + tempDurasi;
+    if (tempTotal < 60) {
+      totalDuration = tempTotal;
+      randomArtist.push(dataLagu[randomIndex]);
+    }else{
+      break;
+    }
+  }
+
+  return randomArtist.concat(`Total Durasi: ${totalDuration} Menit`);
+}
+
+// console.log(groupByArtist(dataLagu, 'noAh1'));
+// console.log(groupByGenre(dataLagu, 'metAl'))
+console.log(groupSongUnder60Minute(dataLagu, 21));
